2d4c164824fa2c514cdea385d137329773effb8a
Alex Brown <alex.brown@ink-global.com>
2013-10-24 13:22:42 +0100

    Added failing tests for #25

--- src/MvcRouteTester.Test/ApiControllers/FromUriController.cs
+++ src/MvcRouteTester.Test/ApiControllers/FromUriController.cs
@@ -1,21 +1,24 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace MvcRouteTester.Test.ApiControllers
 {
-	public class UriDataModel
-	{
-		public string Name { get; set; }
-		public int Number { get; set; }
-		public int? OtherNumber { get; set; }
-	}
+    public class UriDataModel
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int? OtherNumber { get; set; }
 
-	public class FromUriController : ApiController
-	{
-		[HttpGet]
-		public HttpResponseMessage DoSomething([FromUri] UriDataModel data)
-		{
-			return new HttpResponseMessage();
-		}
-	}
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class FromUriController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage DoSomething([FromUri] UriDataModel data)
+        {
+            return new HttpResponseMessage();
+        }
+    }
 }
